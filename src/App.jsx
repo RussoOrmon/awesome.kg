92b78ce8e0412949c75f0abbe72826707a587088
Russo Ormon <tturapov@gmail.com>
2022-10-11 09:11:03 +0300

    dly proby add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<p>
+				Проект: Russo Ormon <b>ot Ruslan</b>
+			</p>
+         <span> для пробы </span>
+		</div>
+	)
+}
+
+export default App
